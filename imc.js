b01753d0a6de77fc0eb8ea7047a89d945d82a3bf
djalminha153 <alvesdjalmacordeiroalves@gmail.com>
2024-11-04 17:39:28 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -13,8 +13,8 @@ function imc(){
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade morbida;
-            if (imc >= 18.5 &&  imc < 25 ) this.classificação = "saudável
+            if (imc < 18.5 ) this.classificacao = "obesidade morbida";
+            if (imc >= 18.5 &&  imc < 25 ) this.classificação = "saudável"
             if (imc >= 25 &&  imc < 30 ) this.classificação = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificação = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificação = "obesidade Severa"
